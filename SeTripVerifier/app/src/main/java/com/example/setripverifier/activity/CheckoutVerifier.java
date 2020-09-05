54e97c6a2af8dbc452ce843e7bfa4671e7a88368
rivaldofez <rivaldofez@gmail.com>
2020-09-06 04:43:45 +0700

    make checkout verifier scanner

---
+++ SeTripVerifier/app/src/main/java/com/example/setripverifier/activity/CheckoutVerifier.java
@@ -0,0 +1,101 @@
+package com.example.setripverifier.activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.example.setripverifier.R;
+import com.example.setripverifier.model.TripModel;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+public class CheckoutVerifier extends AppCompatActivity {
+
+    private static final String TAG = "Test";
+    CodeScanner codeScanner;
+    CodeScannerView scannerView;
+    String uid;
+
+    FirebaseDatabase root;
+    DatabaseReference reference;
+
+    private ValueEventListener valueEventListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout_verifier);
+
+        scannerView = findViewById(R.id.checkinScanner);
+
+        codeScanner = new CodeScanner(this, scannerView);
+
+        codeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        uid = result.getText();
+                    }
+                });
+            }
+        });
+
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                codeScanner.startPreview();
+            }
+        });
+    }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        requestForCamera();
+    }
+
+    private void requestForCamera() {
+        Dexter.withActivity(this).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse response) {
+                codeScanner.startPreview();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse response) {
+                Toast.makeText(CheckinVerifier.this, "Camera Permission is Required.", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                token.continuePermissionRequest();
+            }
+        }).check();
+    }
+
+
+}
